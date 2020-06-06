aedf89a550a73289e67cc0802acffbed4af4956c
Ysobel <ysobelsims@gmail.com>
2020-06-06 19:54:16 +1000

    Add continue train to batch

--- train_soccer.bat
+++ train_soccer.bat
@@ -5,6 +5,7 @@ ECHO OFF
 IF "%~1"=="" ( SET GPU_ID=-1 ) ELSE SET GPU_ID=%~1
 IF "%~2"=="" ( SET BATCH_SIZE=1 ) ELSE SET BATCH_SIZE=%~2
 IF "%~3"=="" ( SET NAME=soccer ) ELSE SET NAME=%~3
+IF "%~4"=="" ( SET CONT=0 ) ELSE SET CONT=%~4
 
 python train.py^
  --dataroot ./datasets/soccer^
@@ -18,3 +19,4 @@ python train.py^
  --no_dropout^
  --load_size 600^
  --crop_size 256^
+ --continue_train %CONT%
