3076dcc3cc2209a5709c1f5f909529045dd03f39
Ysobel <ysobelsims@gmail.com>
2020-06-08 07:22:21 +1000

    Update the training file for blender

--- train_soccer.bat
+++ train_soccer.bat
@@ -4,14 +4,14 @@ ECHO OFF
 
 IF "%~1"=="" ( SET GPU_ID=-1 ) ELSE SET GPU_ID=%~1
 IF "%~2"=="" ( SET BATCH_SIZE=1 ) ELSE SET BATCH_SIZE=%~2
-IF "%~3"=="" ( SET NAME=soccer ) ELSE SET NAME=%~3
+IF "%~3"=="" ( SET NAME=soccer_%DATE% ) ELSE SET NAME=%~3
 IF "%~4"=="" ( SET CONT=0 ) ELSE SET CONT=%~4
 
 python train.py^
- --dataroot ./datasets/soccer^
+ --dataroot ./datasets/soccer_blender_real^
  --name %NAME%^
- --model uag_gan^
- --dataset_mode unaligned^
+ --model uag_gan_updated^
+ --dataset_mode blender^
  --gpu_ids %GPU_ID%^
  --batch_size %BATCH_SIZE%^
  --use_early_stopping 1^
